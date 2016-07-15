cef2521928c47587594288bfc8d9b29d39af2e76
BlueCobra-6 <tlarson6@me.com>
2016-07-15 13:57:54 -0500

    Finding n-th even Fibonacci number
    Problem 1 - Added new file for solving the problem of finding the 1000th number that is a multiple of 3 and 5.
    
    *TO DO*
    - Add comments
    - Rework to allow input of a different n-th number
    - Remove Fibonacci calculator and make it into a standard function so it is isolated and available for reuse

---
+++ problem_2.groovy
@@ -0,0 +1,24 @@
+import java.io.*;
+public class Main {
+  public static void main( String[] ars ) {
+    println new EvenFibonacci().function( 4000000 )
+  }
+}
+
+class EvenFibonacci {
+  def function( int a ) {
+    def total = 0
+    def firstnumber = 1
+    def secondnumber = 1
+    def placeholder = firstnumber
+    while( secondnumber < a ) {
+      if( secondnumber.mod( 2 ) == 0 {
+      total += secondnumber
+      }
+      placeholder = secondnumber
+      secondnumber += firstnumber
+      firstnumber = placeholder
+    }
+    return total
+  }
+}
