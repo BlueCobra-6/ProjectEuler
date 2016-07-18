b9fcb6b30d1fe6f247de79b3850e33c31377cbdb
BlueCobra-6 <tlarson6@me.com>
2016-07-18 15:03:14 -0500

    Fixed bugs in grammar
    Added a open bracket for the differenceSumofSquaresandSquareofSum class and fixed the call of the main function.

--- problem_6.groovy
+++ problem_6.groovy
@@ -1,11 +1,11 @@
 import java.io.*;  
 public class Main {  
   public static void main(String[] args) {  
-    println new defferenceSumofSquaresandSquareofSum().difference(100)  
+    println new differenceSumofSquaresandSquareofSum().difference(100)  
   }  
 }  
 
-class differenceSumofSquaresandSquareofSum 
+class differenceSumofSquaresandSquareofSum {
   def difference( int a ) {
     return SquareofSum(a) - SumofSquares(a)
   }
